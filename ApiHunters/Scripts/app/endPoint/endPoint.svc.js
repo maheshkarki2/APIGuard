7f377dec84cc4d291a7bbf5c9274161f388bbf35
Mahesh Karki <MKarki@napower.com>
2016-12-20 15:01:44 -0500

    Get Request and populate to the response body

--- ApiHunters/Scripts/app/endPoint/endPoint.svc.js
+++ ApiHunters/Scripts/app/endPoint/endPoint.svc.js
@@ -2,18 +2,48 @@
     'use strict';
 
     // register service as a factory
-    angular.module('app').factory('salesService', salesService);
+    angular.module('app').factory('salesService', salesServiceDef);
 
-    salesService.$inject = ['$resource'];
-    var headers = {};
-    function salesService($resource, url) {
-
-        return $resource(url,
-        { id: '@id' },
+    salesServiceDef.$inject = ['$resource'];
+   
+    function salesServiceDef($resource) {
+        var salesService = {};
+        salesService.getRequest = getRequestService2;
+        salesService.postRequest = PostRequestService;
+        function getRequestService(passurl,requestBody, customheaders)
         {
-            'query': { isArray: false },
-            'getRequest': { 'method': 'GET', 'url': url, 'headers': {'authorization': headers.authToken}},       
-            'postRequest': { 'method': 'POST', 'url': url, 'headers': { 'authorization': headers.authToken }}
-        });
+            return $resource(passurl,
+                null,
+                {
+                    GET : {
+                        method: 'GET',
+                        url:passurl,
+                        headers: { 'authorization': customheaders.authTokenValue }
+                    }
+                });
+        }
+       function getRequestService2(passurl, requestBody, customeheaders)
+        {
+            return $resource(passurl, null, {}, { headers: { 'Authorization': customeheaders.authTokenValue } }).get(null, null);                     
+        }
+        function PostRequestService(url, requestBody, headers) {
+            return $resource(url,
+                { /*define params here ex- Id: '@Id' */ },
+                {
+                    post: {
+                        method: 'POST',
+                        headers: { 'Authorization': customheaders.authTokenValue }
+                    }
+                });
+        }
+
+        return salesService;
+        //return $resource(url,
+        //{ id: '@id' },
+        // {
+        //    'query': { isArray: false },
+        //    'getRequest': { 'method': 'GET', 'url': url, 'headers': {'authorization': headers.authToken}},       
+        //    'postRequest': { 'method': 'POST', 'url': url, 'headers': { 'authorization': headers.authToken }}
+        //});
     }
 })();
--- ApiHunters/Scripts/app/endPoint/endPointCtrl.js
+++ ApiHunters/Scripts/app/endPoint/endPointCtrl.js
@@ -16,26 +16,33 @@
 	    $scope.title = 'apiEndpoint';
 
 	    activate();
-	    var headers = {};
-
+	    $scope.customeHeaders = { authTokenHeader: null, authTokenValue: null };
+	    $scope.inputData = [];
 	    $scope.value = 1;
 	    $scope.headerActive = "active";
 	    $scope.requestActive = "inactive";
 	    $scope.responseActive = "inactive";
-	    $scope.HitApi=function(auth, authToken)
+	    $scope.HitApi=function()
 	    {
-	        headers={authToken: authToken};
-	        var getRequest = salesService.getRequest($scope.url, headers).$promise;
-            
+	        $scope.customHeaders=$scope.ComposeHeader();
+	        var getRequest = salesService.getRequest($scope.url, null, $scope.customHeaders).$promise;
+	        getRequest.then(function (success) {
+	            $scope.JsonResponse = JSON.stringify(success, null, 2);
+	            SelectTab(3);
+	        },
+                function (error) {
+                    $scope.JsonResponse = JSON.stringify(error, null, 2);
+                    $scope.SelectTab(3);
+                });
 	    }
 
 	    function activate() { }
 
-	    $scope.choices = [{ id: 'choice1' }];
+	    $scope.choices = [{ id: 'choice1'}];
 
 	    $scope.addNewChoice = function () {
 	        var newItemNo = $scope.choices.length + 1;
-	        $scope.choices.push({ 'id': 'choice' + newItemNo });
+	        $scope.choices.push({ 'id': 'choice' + newItemNo});
 	    };
 
 	    $scope.removeChoice = function () {
@@ -50,16 +57,25 @@
 	            $scope.requestActive = "inactive";
 	            $scope.responseActive = "inactive";
 	        }
-	        if (data == 2){
+	        if (data == 2) {
 	            $scope.requestActive = "active";
 	            $scope.headerActive = "inactive";
 	            $scope.responseActive = "inactive";
-            }
-	        if (data == 3){
+	        }
+	        if (data == 3) {
 	            $scope.responseActive = "active";
 	            $scope.headerActive = "inactive";
 	            $scope.requestActive = "inactive";
-            }
-	    }
+	        }
+	    };
+
+	    $scope.ComposeHeader = function () {
+	        $scope.choices.forEach(function (data) {
+	            $scope.customeHeaders.authTokenHeader = 'Authorization';
+	            $scope.customeHeaders.authTokenValue = 'bearer '+data.headerValue;
+	        });
+	        return $scope.customeHeaders;
+            
+	    };
 	}
 })();
